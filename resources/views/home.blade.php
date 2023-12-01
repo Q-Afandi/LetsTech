0563b55440c6a8917f243de7776ca0dee2a60669
vinnnaaa <bellarevina23@gmail.com>
2023-12-01 20:04:01 +0700

    navbar-jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,5 @@
 @section('container')
 
 
+
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#265073",
+                secondary: "#0000",
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
