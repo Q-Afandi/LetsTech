0dc2feb3ac466bd5a257aebb2b8ac3dbf8b0c6f7
vinnnaaa <bellarevina23@gmail.com>
2023-12-30 00:03:30 +0700

    halaman_produk

---
+++ resources/views/index.blade.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <nav class="nav" style="background-color: #e3f2fd;">
+        <div class="container navbar">
+            <a href="#">Gadget Indonesia</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1 class="title">Temukan ponsel yang tepat untuk Anda</h1>
+
+        <div class="card" style="margin-top: 1rem;margin-bottom: 1rem;">
+            <ul class="list-phone">
+                <li data-filter=".all" class="products__item list-phone-item active-list-phone-item">
+                    iphone
+                </li>
+                <li data-filter=".samsung" class="products__item list-phone-item">samsung</li>
+                <li data-filter=".ovo" class="products__item list-phone-item">ovo</li>
+            </ul>
+        </div>
+
+        <div class="grid products__content">
+            <div class="col products__card all">
+                <div class="card">
+                    <span
+                        style="padding: .5rem 1.25rem;border-radius: 0 0.375rem 0 0.375rem;position: absolute;top:0;right:0;background-color: #f55a3e;color: #fff;">12%</span>
+                    <img src="https://cdn.jagofon.com/model/wzSSlEpbEtwNEFgNZD1MMQLXV1DVWqYDJYrHYTDT.jpg" alt="">
+                    <h5 style="color: #242933;margin-top: .5rem;font-size: 1.25rem;">iPhone 13 Mini - 256 GB - Pink</h5>
+                    <p style="margin-top: .25rem;"> <span style="font-weight: 700;margin-top: 1rem;">Kondisi:</span>
+                        Mint
+                    </p>
+                    <div style=" margin-top: .25rem;">
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bx-star'></i>
+                    </div>
+                    <p style="margin-top: .5rem;font-size: 1.5rem;">Rp 13.700.000 <s
+                            style="color: #999;font-size: .75rem" class="product-item__price-new" data-v-26524ea4="">
+                            Rp&nbsp;15.499.000
+                        </s></p>
+                    <div style="display: flex;column-gap: .5rem;margin-top: .5rem;">
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">Resmi Indonesia
+                        </div>
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">IMEI Verified
+                        </div>
+                    </div>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=+62877-5240-6734&text=saya mau pesan"
+                        style="padding: .75rem 1rem;margin-top: 1rem;display: block;text-decoration: none; text-align: center;"
+                        class="btn">Pesan Sekarang</a>
+                </div>
+            </div>
+            <div class="col products__card all ovo">
+                <div class="card" style="box-shadow: 0 30px 40px 0 rgba(212, 217, 232, .3);">
+                    <span
+                        style="padding: .5rem 1.25rem;border-radius: 0 0.375rem 0 0.375rem;position: absolute;top:0;right:0;background-color: #f55a3e;color: #fff;">12%</span>
+                    <img src="https://cdn.jagofon.com/model/jNkRTWWloYV6uBo3cUwL8C8ZCpRYinbnk1uU9pnn.jpg" alt="">
+                    <h5 style="color: #242933;margin-top: .5rem;font-size: 1.25rem;">iPhone 13 Mini - 256 GB - Pink</h5>
+                    <p style="margin-top: .25rem;"> <span style="font-weight: 700;margin-top: 1rem;">Kondisi:</span>
+                        Mint
+                    </p>
+                    <div style=" margin-top: .25rem;">
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bx-star'></i>
+                    </div>
+                    <p style="margin-top: .5rem;font-size: 1.5rem;">Rp 13.700.000 <s
+                            style="color: #999;font-size: .75rem" class="product-item__price-new" data-v-26524ea4="">
+                            Rp&nbsp;15.499.000
+                        </s></p>
+                    <div style="display: flex;column-gap: .5rem;margin-top: .5rem;">
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">Resmi Indonesia
+                        </div>
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">IMEI Verified
+                        </div>
+                    </div>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=+62877-5240-6734&text=saya mau pesan"
+                        style="padding: .75rem 1rem;margin-top: 1rem;display: block;text-decoration: none; text-align: center;"
+                        class="btn">Pesan Sekarang</a>
+                </div>
+            </div>
+            <div class="col products__card all samsung">
+                <div class="card" style="box-shadow: 0 30px 40px 0 rgba(212, 217, 232, .3);">
+                    <span
+                        style="padding: .5rem 1.25rem;border-radius: 0 0.375rem 0 0.375rem;position: absolute;top:0;right:0;background-color: #f55a3e;color: #fff;">12%</span>
+                    <img src="https://cdn.jagofon.com/model/a268wUMA1P7UAKUQd1YN4lZQ7d2aYEsgCttSuDTu.png" alt="">
+                    <h5 style="color: #242933;margin-top: .5rem;font-size: 1.25rem;">iPhone 13 Mini - 256 GB - Pink</h5>
+                    <p style="margin-top: .25rem;"> <span style="font-weight: 700;margin-top: 1rem;">Kondisi:</span>
+                        Mint
+                    </p>
+                    <div style=" margin-top: .25rem;">
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bx-star'></i>
+                    </div>
+                    <p style="margin-top: .5rem;font-size: 1.5rem;">Rp 13.700.000 <s
+                            style="color: #999;font-size: .75rem" class="product-item__price-new" data-v-26524ea4="">
+                            Rp&nbsp;15.499.000
+                        </s></p>
+                    <div style="display: flex;column-gap: .5rem;margin-top: .5rem;">
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">Resmi Indonesia
+                        </div>
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">IMEI Verified
+                        </div>
+                    </div>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=+62877-5240-6734&text=saya mau pesan"
+                        style="padding: .75rem 1rem;margin-top: 1rem;display: block;text-decoration: none; text-align: center;"
+                        class="btn">Pesan Sekarang</a>
+                </div>
+            </div>
+            <div class="col products__card all samsung">
+                <div class="card" style="box-shadow: 0 30px 40px 0 rgba(212, 217, 232, .3);">
+                    <span
+                        style="padding: .5rem 1.25rem;border-radius: 0 0.375rem 0 0.375rem;position: absolute;top:0;right:0;background-color: #f55a3e;color: #fff;">12%</span>
+                    <img src="https://cdn.jagofon.com/model/YoW2AOkOEuJbZrcGJE1kHIpVNdM27sT1JiZYRUEi.jpg" alt="">
+                    <h5 style="color: #242933;margin-top: .5rem;font-size: 1.25rem;">iPhone 13 Mini - 256 GB - Pink</h5>
+                    <p style="margin-top: .25rem;"> <span style="font-weight: 700;margin-top: 1rem;">Kondisi:</span>
+                        Mint
+                    </p>
+                    <div style=" margin-top: .25rem;">
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bxs-star'></i>
+                        <i style="color: #FBD048;font-size: 1rem;" class='bx bx-star'></i>
+                    </div>
+                    <p style="margin-top: .5rem;font-size: 1.5rem;">Rp 13.700.000 <s
+                            style="color: #999;font-size: .75rem" class="product-item__price-new" data-v-26524ea4="">
+                            Rp&nbsp;15.499.000
+                        </s></p>
+                    <div style="display: flex;column-gap: .5rem;margin-top: .5rem;">
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">Resmi Indonesia
+                        </div>
+                        <div style="background-color: #f5f5f5;padding: .5rem;border-radius: .25rem;">IMEI Verified
+                        </div>
+                    </div>
+                    <a target="_blank" href="https://api.whatsapp.com/send/?phone=+62877-5240-6734&text=saya mau pesan"
+                        style="padding: .75rem 1rem;margin-top: 1rem;display: block;text-decoration: none; text-align: center;"
+                        class="btn">Pesan Sekarang</a>
+                </div>
+            </div>
+        </div>
+
+        <h1 class="title" style="margin-top: 6rem;">Berbagai Cerita Dari #ReviewerGadget</h1>
+        <div class="card__container swiper">
+            <div class="card__content">
+                <div class="swiper-wrapper">
+                    <article class="card__article swiper-slide">
+                        <div class="card">
+                            <iframe class="rounded" style="height: 300px;"
+                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                            </iframe>
+                        </div>
+                    </article>
+
+                    <article class="card__article swiper-slide">
+                        <div class="card">
+                            <iframe class="rounded" style="height: 300px;"
+                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                            </iframe>
+                        </div>
+                    </article>
+
+                    <article class="card__article swiper-slide">
+                        <div class="card">
+                            <iframe class="rounded" style="height: 300px;"
+                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                            </iframe>
+                        </div>
+                    </article>
+
+                    <article class="card__article swiper-slide">
+                        <div class="card">
+                            <iframe class="rounded" style="height: 300px;"
+                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                            </iframe>
+                        </div>
+                    </article>
+
+                    <article class="card__article swiper-slide">
+                        <div class="card">
+                            <iframe class="rounded" style="height: 300px;"
+                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                            </iframe>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+            <!-- Navigation buttons -->
+            <div class="swiper-button-next">
+                <i class='bx bx-chevron-right'></i>
+            </div>
+
+            <div class="swiper-button-prev">
+                <i class='bx bx-chevron-left'></i>
+            </div>
+
+            <!-- Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
+    <footer>
+        copyright 2024
+    </footer>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/mixitup.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
