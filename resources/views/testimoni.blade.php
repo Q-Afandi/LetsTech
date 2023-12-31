89e6421b4ae1b25ef81850b1630ff0898be2b00b
vinnnaaa <bellarevina23@gmail.com>
2023-12-31 16:32:29 +0700

    halaman_testimoni

--- resources/views/testimoni.blade.php
+++ resources/views/testimoni.blade.php
@@ -1,65 +1,46 @@
 @extends('layouts.main')
 
 @section('container')
-<!-- testimoni -->
-<section id="testimoni">
-<h1 class="text-center mt-5">Berbagai Cerita Dari #ReviewerGadget</h1>
-        <div class="card__container swiper">
-            <div class="card__content">
-                <div class="swiper-wrapper">
-                    <article class="card__article swiper-slide">
-                        <div class="card">
-                            <iframe class="rounded" style="height: 300px;"
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-                    </article>
 
-                    <article class="card__article swiper-slide">
-                        <div class="card">
-                            <iframe class="rounded" style="height: 300px;"
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-                    </article>
-
-                    <article class="card__article swiper-slide">
-                        <div class="card">
-                            <iframe class="rounded" style="height: 300px;"
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-                    </article>
-
-                    <article class="card__article swiper-slide">
-                        <div class="card">
-                            <iframe class="rounded" style="height: 300px;"
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-                    </article>
-
-                    <article class="card__article swiper-slide">
-                        <div class="card">
-                            <iframe class="rounded" style="height: 300px;"
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                        </div>
-                    </article>
+<!-- review -->
+<section>
+    <div class="container mx-auto py-12 pt-15 mt-24">
+        <h3 class="xl:text-center text-xl font-semibold ">Review Dari Customer </h3>
+        <div class="xl:flex gap-3 mt-5">
+            <div class="mx-auto p-10 rounded-xl shadow-lg">
+                <div class="xl:flex">
+                    <div class="w-1/4">
+                        <img class ="w-10" src="/src/img/testimoni1.png" alt>
+                    </div>
+                    <h4 class="text-left w-3/4 font-bold">Freya</h4>
                 </div>
+                <p class="mt-2">
+                    Layanannya sangat oke dan rekomen untuk membeli handphone
+                </p>
             </div>
-
-            <!-- Navigation buttons -->
-            <div class="swiper-button-next">
-                <i class='bx bx-chevron-right'></i>
+            <div class="mx-auto p-10 rounded-xl shadow-lg">
+                <div class="xl:flex">
+                    <div class="w-1/4">
+                        <img class ="w-10" src="/src/img/testimoni2.png" alt>
+                    </div>
+                    <h4 class="w-3/4 font-bold">Ucup</h4>
+                </div>
+                <p class="mt-2">
+                    Selama pemakaian 3 bulan ini aman-aman saja tidak ada kendala apapun.
+                </p>
             </div>
-
-            <div class="swiper-button-prev">
-                <i class='bx bx-chevron-left'></i>
+            <div class="mx-auto p-10 rounded-xl shadow-lg">
+                <div class="xl:flex">
+                    <div class="w-1/4">
+                        <img class ="w-10" src="/src/img/testimoni3.png" alt>
+                    </div>
+                    <h4 class="w-3/4 font-bold">Susi</h4>
+                </div>
+                <p class="mt-2">
+                    harga yang ditawar cukup terjangkau dan service cukup memuaskan.
+                </p>
             </div>
-
-            <!-- Pagination -->
-            <div class="swiper-pagination"></div>
+        </div>
         </div>
     </div>
 </section>
